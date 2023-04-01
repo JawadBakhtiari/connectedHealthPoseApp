e1998c073d6a4e0ebab3a3b5a6dcfb3b5120ccef
Kifaya Shehadeh <z5421904@ad.unsw.edu.au>
2023-04-02 01:12:05 +1100

    basic 2d animation using sample data and OPENCV

--- data/urls.py
+++ data/urls.py
@@ -5,5 +5,4 @@ app_name = 'data'
 
 urlpatterns = [
      path('visualise/', views.visualise_coordinates, name='visualise_coordinates'),
-     path('visualise_coordinates/', views.visualise_coordinates, name='visualise_coordinates'),
 ]
