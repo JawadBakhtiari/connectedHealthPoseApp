c756fe99c26974c01fd6de5ed42765e36d22dcdd
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2023-05-29 15:22:11 +1000

    rename root app

--- connectedhealth/asgi.py
+++ connectedhealth/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HumanPose3D.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connectedhealth.settings')
 
 application = get_asgi_application()
