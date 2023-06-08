08e4139a111c21e9eb4b790fb1cb452223ff05db
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2023-06-09 09:30:57 +1000

    remove old project name from docstrings

--- connectedhealth/urls.py
+++ connectedhealth/urls.py
@@ -1,4 +1,4 @@
-"""framestore_test URL Configuration
+"""connectedhealth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
