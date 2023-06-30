25045381a59be7e5d67c22a07f83bbff285f1594
Your Name <email@example.com>
2023-06-30 14:58:59 +1000

    added code documents, and increased fps to 10

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { StyleSheet, Text, View, Platform, Image, Button } from 'react-native';
 import { Camera } from 'expo-camera';
+import { GLView } from 'expo-gl';
 import * as tf from '@tensorflow/tfjs';
 import * as posedetection from '@tensorflow-models/pose-detection';
 import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
@@ -13,7 +14,7 @@ const TensorCamera = cameraWithTensors(Camera);
 
 const IS_IOS = Platform.OS === 'ios';
 
-const OUTPUT_TENSOR_WIDTH = 180;
+const OUTPUT_TENSOR_WIDTH = 96;
 const OUTPUT_TENSOR_HEIGHT = OUTPUT_TENSOR_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
 
 
@@ -28,13 +29,30 @@ export default function App() {
   const rafId = useRef(null);
   const [fps, setFps] = useState(0);
   
+  /**
+   * This is a React useEffect hook that runs once when the component is mounted.
+   * The empty dependency array ([]) ensures that the effect only runs once.
+   * Inside this effect, an asynchronous function prepare() is defined.
+  **/
   useEffect(() => {
+
+    /**
+     * This is an asynchronous function named prepare(), defined inside the useEffect hook.
+     * It initializes the camera permissions, waits for TensorFlow.js to be ready, 
+     * and prepares the pose detection model.
+    **/
     async function prepare() {
       rafId.current = null;
 
       await Camera.requestCameraPermissionsAsync();
       await tf.ready();
-      
+      /**
+       * Pose Detection Setup:
+       * The code sets up the configuration object for the pose detector, specifying runtime, smoothing, and model type.
+       * It then calls the createDetector() function provided by the posedetection module to create the pose detection model.
+       * The created model is stored in the state using the setModel() function.
+       * The state variable tfReady is set to true using the setTfReady() function.
+      **/
       const detectorConfig = {
         runtime: 'tfjs',
         enableSmoothing: true,
@@ -51,8 +69,18 @@ export default function App() {
     prepare();
   }, []);
 
+  /**
+     * This is another useEffect hook that runs once when the component is mounted.
+     * The empty dependency array ([]) ensures that the effect only runs once.
+     * Inside this effect, a cleanup function is defined.
+  **/
   useEffect(() => {
     return () => {
+      /**
+       * The cleanup function checks if the rafId (requestAnimationFrame ID) is set 
+       * and cancels the animation frame using cancelAnimationFrame() if needed.
+       * It ensures that any pending animation frames are cleared when the component is unmounted.
+      **/
       if (rafId.current != null && rafId.current !== 0) {
         cancelAnimationFrame(rafId.current);
         rafId.current = 0;
@@ -60,62 +88,49 @@ export default function App() {
     };
   }, []);
 
-  const handleCameraStream = async (images) => {
-   
-    const loop = async () => {
+  /**
+   * This toggles the isRecording state variable using the setIsRecording() function.
+  **/
+  const handleRecording = () => {
+    setIsRecording(prevRecording => !prevRecording)
+  };
+
+  /**
+   * Takes an images parameter.
+   * Responsible for processing the camera stream and running the BlazePose model on the captured images.
+  **/
+  const handleCameraStream = (images) => {
+
+    const loop = () => {
+      /**
+       * This line retrieves the next image from the images object using the next() method.
+       * The value property contains the actual image tensor data.
+      **/
       const tensor = images.next().value;
-      //console.log(isRecording)
-      //encodeJpeg(tensor)
-      //loadModel(tensor)
-
+      // This marks the starting time of the image processing for measuring latency.
       const startTs = Date.now();
-    
-      const [height, width] = tensor.shape
-      const data = new Buffer.from(
-        tf.concat([tensor, tf.ones([height, width, 1]).mul(255)], [-1])
-          .slice([0], [height, width, 4])
-          .dataSync(),
-      )
-      const rawImageData = {data, width, height};
-      const jpegImageData = jpeg.encode(rawImageData, 200);
 
-      const imgBase64 = tf.util.decodeString(jpegImageData.data, "base64")
-      const salt = `${Date.now()}-${Math.floor(Math.random() * 10000)}`
-      const uri = FileSystem.documentDirectory + `tensor-${salt}.jpg`;
-      await FileSystem.writeAsStringAsync(uri, imgBase64, {
-        encoding: FileSystem.EncodingType.Base64,
-      });
-      setUri(uri)
-      
-
-      const poses = await model.estimatePoses(
-        tensor,
-        undefined,
-        Date.now()
-      );
-      
+      encodeJpeg(tensor)
+      loadModel(tensor) 
+      // Calculates the latency in order to get the FPS  
       const latency = Date.now() - startTs;
       setFps(Math.floor(1000 / latency));
 
-      setPoses(poses);
-      let data2 = poses.find(poses => poses.keypoints)
-      //console.log(uri)
-      //console.log(data2)
-
+      // Disposes image tensoor to free memery resources after used
       tf.dispose([tensor]);
-  
+
+      // If current animation frame is no longer processing then exit
       if (rafId.current === 0) {
         return;
       }
-  
       rafId.current = requestAnimationFrame(loop);
     };
-  
     loop();
     
   };
 
-  const loadModel = async () => {
+
+  const loadModel = async (tensor) => {
     const poses = await model.estimatePoses(
       tensor,
       undefined,
@@ -132,17 +147,36 @@ export default function App() {
     );
   };
 
-  const encodeJpeg = async () => {
- 
+  const encodeJpeg = async (tensor) => {
+    
+    /**
+     * Prepare RGB Image Data
+     * Concatenate the input tensor with an alpha channel tensor (if necessary) and extract RGB pixel values
+     * Returns a flat array of RGB pixel values representing the image data
+    **/
     const [height, width] = tensor.shape
     const data = new Buffer.from(
       tf.concat([tensor, tf.ones([height, width, 1]).mul(255)], [-1])
         .slice([0], [height, width, 4])
         .dataSync(),
     )
+    /**
+     * The jpeg.encode() function is called with two arguments: rawImageData and 100.
+     * rawImageData: This object contains the raw image data, width, and height of the image.
+     * 100: This parameter represents the quality setting for JPEG encoding. 
+     * Higher values (up to 100) indicate better quality but larger file sizes.
+    **/
     const rawImageData = {data, width, height};
-    const jpegImageData = jpeg.encode(rawImageData, 200);
-
+    const jpegImageData = jpeg.encode(rawImageData, 100);
+    
+     /**
+     * Convert Encoded Image to Base64 and Save to File
+     *  - Decode the base64-encoded image data from `jpegImageData.data`
+     * - Generate a unique file name with a timestamp and random number (`salt`)
+     * - Create the URI for the file in the document directory
+     * - Write the base64-encoded image data to the file using `FileSystem.writeAsStringAsync()`
+     * The resulting URI can be used to access the saved image file
+    **/
     const imgBase64 = tf.util.decodeString(jpegImageData.data, "base64")
     const salt = `${Date.now()}-${Math.floor(Math.random() * 10000)}`
     const uri = FileSystem.documentDirectory + `tensor-${salt}.jpg`;
@@ -150,21 +184,21 @@ export default function App() {
       encoding: FileSystem.EncodingType.Base64,
     });
     setUri(uri)
-    console.log(uri)
-    
-    //await MediaLibrary.saveToLibraryAsync(uri); To test result off the tensor image
+
+    //await MediaLibrary.saveToLibraryAsync(uri); // To test result of the tensor image
   };
   
 
   const renderPose = () => {
-    if (poses != null && poses.length > 0 && isRecording) {
-      let data = poses.find(poses => poses.keypoints)
+    if (poses != null && poses.length > 0) {
+
+      let data = poses.find(poses => poses.keypoints3D)
+      data["frame_image"] = uri
       console.log(data)
  
     }
   };
   
- 
   
   const renderCameraTypeSwitcher = () => {
     return (
@@ -199,15 +233,7 @@ export default function App() {
     );
   };
 
-  const handleRecording = () => {
-    if (!isRecording) {
-      setIsRecording(true);
-    } else {
-      setIsRecording(false)
-    }
 
-  };
- 
   
   if (!tfReady) {
     return (
@@ -230,9 +256,11 @@ export default function App() {
           resizeDepth={3}
           onReady={handleCameraStream}
         />
+        {renderPose()}
         {renderFps()}
         {renderCameraTypeSwitcher()}
         {renderRecordButton()}
+      
       </View>
     );
   }}
