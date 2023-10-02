bd8847e920bf8de623d030b10ff49f54f848205a
Swoggles <50952988+Swoggles@users.noreply.github.com>
2023-10-02 14:11:00 +1100

    added button template

--- front-end/pages/UploadingScreen.jsx
+++ front-end/pages/UploadingScreen.jsx
@@ -1,7 +1,30 @@
+import * as React from 'react';
+import TouchableOpacity from 'react-native';
 
 
+export default function UploadingScreen({ uploading }) {
+  const [isDisabled, setIsDisabled] = useState(false)
 
-  export default function SecondScreen({ uploading }) {
-
-
+  const checkUploadStatus = () => {
+    setIsDisabled(false)
   }
+
+  return (
+  <TouchableOpacity
+    onPress={() =>
+      navigation.navigate("HomeScreen", { language: "french" })
+    }
+    style={[
+      styles.submitBtn,
+      { backgroundColor: isValid ? "#B05D5D" : "#BB9393" },
+    ]}
+    disabled={isDisabled}
+  >
+    <Text style={[styles.submitBtnTxt]}>Return to Main Menu</Text>
+  </TouchableOpacity>
+
+  )
+  // Finish Button -> Greyed out
+  // Check for any frames that still need to be uploaded to backend
+  // Once done - allow user to press finish back which navigates back to home screen.
+}
