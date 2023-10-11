f4124a4ad9c8a77663e485c73b10c31709f19d96
Yusra <z5417725@ad.unsw.edu.au>
2023-10-11 11:32:48 +1100

    2D overlay of coordinates

--- data/views.py
+++ data/views.py
@@ -23,6 +23,12 @@ import base64
 import data.const as const
 from io import BytesIO
 
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
 def dashboard(request):
     user = request.user
     involvements = InvolvedIn.objects.filter(user=user)
@@ -134,7 +140,7 @@ def visualise_2D(request):
     #           don't expect user id in request currently
 
     # use sample data if request is empty (happens when page is first loaded by url)
-    sid = "e73b5edc-7f2f-43ae-acd6-f2b68b3d0497"
+    sid = "12983129"
     clip_num = "1"
     if request.body:
         # if request non-empty, use this data
@@ -157,37 +163,48 @@ def visualise_2D(request):
 
     # overlaying pose data on image data
     frames = []
-    for p in store.get_poses():
-        ret, frame_image = cap.read()
-        if not ret:
-            # video has reached the end
-            break
+    # Create a Pose model for static images
+    with mp_pose.Pose(
+        static_image_mode=True,
+        model_complexity=2,
+        enable_segmentation=True,
+        min_detection_confidence=0.5) as pose:
 
-        frame_height, frame_width, _ = frame_image.shape
+        for p in store.get_poses():
+            ret, frame_image = cap.read()
+            if not ret:
+                # video has reached the end
+                break
 
-        # Create a copy of the frame for overlaying keypoints
-        overlay_image = frame_image.copy()
+            frame_height, frame_width, _ = frame_image.shape
 
-        # Overlay each keypoint onto the image for this frame
-        keypoints = []
-        for kp in p.get("keypoints3D"):
-            x, y = int(kp['x'] * frame_width + frame_width/2), int(kp['y'] * frame_height + frame_height/2)
-            keypoints.append((x, y))
-            cv2.circle(overlay_image, (x, y), radius=2, color=(0, 255, 0), thickness=-1)
+            # Convert the BGR image to RGB before processing.
+            results = pose.process(cv2.cvtColor(frame_image, cv2.COLOR_BGR2RGB))
 
-        # Connect the dots - draw lines between joints to form a human stick-figure shape
-        for joint1, joint2 in const.KP_CONNS:
-            pt1 = keypoints[joint1]
-            pt2 = keypoints[joint2]
-            cv2.line(overlay_image, pt1, pt2, (0, 255, 0), 1)
+            # Create a copy of the frame for overlaying keypoints
+            overlay_image = frame_image.copy()
 
-        _, buffer = cv2.imencode('.png', overlay_image)
-        img_base64 = base64.b64encode(buffer).decode('utf-8')
-        frames.append(img_base64)
+            if results.pose_landmarks:
+                # Overlay each keypoint onto the image for this frame
+                keypoints = [(int(lm.x * frame_width), int(lm.y * frame_height)) for lm in results.pose_landmarks.landmark]
+
+                for kp in keypoints:
+                    cv2.circle(overlay_image, kp, radius=2, color=(0, 255, 0), thickness=-1)
+
+                # Connect the dots - draw lines between joints to form a human stick-figure shape
+                for joint1, joint2 in const.KP_CONNS:
+                    pt1 = keypoints[joint1]
+                    pt2 = keypoints[joint2]
+                    cv2.line(overlay_image, pt1, pt2, (0, 255, 0), 1)
+
+            _, buffer = cv2.imencode('.png', overlay_image)
+            img_base64 = base64.b64encode(buffer).decode('utf-8')
+            frames.append(img_base64)
 
     cv2.destroyAllWindows()
     frames = json.dumps(frames)
     return render(request, 'animation.html', {'frames': frames})
+
 
 
 # WORK IN PROGRESS 3D VISUALISATION
