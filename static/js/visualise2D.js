0be8117e8df16d243f71c60538f4aa218ff21a4c
Leo KING <127772787+leoli88@users.noreply.github.com>
2023-10-27 22:00:01 +1100

    Restored visualise2D

--- static/js/visualise2D.js
+++ static/js/visualise2D.js
@@ -12,33 +12,22 @@ function requestVisualisation(sessionId, clipNum) {
     window.location.href = url;
 }
 
-export const doVisualisation = (frameData, chartData) => {
+export const doVisualisation = (frameData) => {
     /* Code to display the visualisation (create a video display) */
     let currentFrame = 0;
     
     function updateFrame() {
-        const frameBase64Img = frameData[currentFrame];
-        if (!frameBase64Img) {
-            console.error('Invalid base64 image data:', frameBase64Img);
+        const base64Img = frameData[currentFrame];
+        if (!base64Img) {
+            console.error('Invalid base64 image data:', base64Img);
             console.error('Current frame:', currentFrame);
             console.error('Frame data:', frameData);
             return;
         }
-
-        const chartBase46Img = chartData[currentFrame];
-        if (!chartBase46Img) {
-            console.error('Invalid base64 chart data:', chartBase46Img);
-            console.error('Current frame:', currentFrame);
-            console.error('Frame data:', chartData);
-            return;
-        }
         
-        const image = document.getElementById("animation");
-        image.src = "data:image/png;base64," + frameBase64Img;
-
-        const chart = document.getElementById(chart)
-        chart.src =  "data:image/png;base64," + chartBase46Img
-
+        const img = document.getElementById("animation");
+        img.src = "data:image/png;base64," + base64Img;
+        
         currentFrame++;
         if (currentFrame >= frameData.length) {
             currentFrame = 0;
