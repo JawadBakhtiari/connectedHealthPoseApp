1dd47737585b0f7c3f627628a57ef45a6758f809
Leo KING <127772787+leoli88@users.noreply.github.com>
2023-10-27 16:46:54 +1100

    Ready for graph demo

--- chart/Parser.py
+++ chart/Parser.py
@@ -1,22 +1,9 @@
-# Imports
-import json
-
 # Parser class
 class Parser:
     # Instantiate class
-    # @params joint, filename, shouldIgnore, shouldWarn
-    def __init__(self, joint, filename, shouldIgnore, shouldWarn) -> None:
-        # Instance variables for joint, input filename and error checking
-        self.joint = joint.lower()
-        self.filename = filename
-        self.shouldIgnore = shouldIgnore
-        self.shouldWarn = shouldWarn
-        self.hasErrors = False
-
-        # Number of frames
-        self.numFrames = 0
-
-        # Arrays for keypoints
+    def __init__(self, joint, poseData) -> None:
+        # Get joint, keypoint and frame data
+        self.joint = joint
         self.leftWristPoints = []
         self.leftElbowPoints = []
         self.leftShoulderPoints = []
@@ -29,73 +16,16 @@ class Parser:
         self.rightHipPoints = []
         self.rightKneePoints = []
         self.rightAnklePoints = []  
-
-        # Open json file
-        try:
-            with open(filename) as inputFile:
-                # Parse json file into dictionary
-                self.data = json.load(inputFile)
-        
-        except:
-            print('File Error: ' + filename + ' cannot be read')
-            exit(1)
-
-        # Error checking for keypoints and joints
-        if not self.isJointValid():
-            print('Parse Error: joint (' + self.joint + ') is invalid')
-            exit(1)
-
-        if not self.arePointsValid() and not self.shouldIgnore:
-            print('Parse Error: keypoints in a frame are invalid')
-            print('Note: There may be duplicate or non-existant keypoints')
-            exit(1)
-
-        # Get points for all joints
+        self.data = poseData
         self.loadPoints()
 
 
-    # Check if joint is valid
-    def isJointValid(self):
-        if self.joint in ['elbow', 'shoulder', 'hip', 'knee']:
-            return True
-        return False
-
-
-    # Check if input file has keypoint errors
-    def arePointsValid(self):
-        frames = self.data['frames']
-        numPoints = len(frames['1'])
-
-        for frame in frames:
-            # Check if number of keypoints in frame is valid
-            if not len(frames[frame]) == numPoints:
-                self.hasErrors = True
-                return False
-            
-            # Check if number of elements in keypoint is valid
-            for keypoint in frames[frame]:
-                if not len(keypoint) == 5:
-                    self.hasErrors = True
-                    return False
-
-        return True
-
-
-    # Load points from file
+    # Load points
     def loadPoints(self):
-        # Get dictionary frames
-        frames = self.data['frames']
-
-        # Get number of frames
-        self.numFrames = len(frames)
-
         # Get all joint points by looping through frames
-        for frame in frames:
-            # Get the current frame
-            current_frame = frames[frame]
-
+        for current_frame in self.data:
             # Add joint points to respective arrays
-            for points in current_frame:
+            for points in current_frame["keypoints"]:
                 if (points['name'] == 'left_wrist'):
                     self.leftWristPoints.append(points)
 
@@ -132,152 +62,9 @@ class Parser:
                 if (points['name'] == 'right_ankle'):
                     self.rightAnklePoints.append(points)
 
-            if self.shouldIgnore:
-                self.fix(frame)
-
-
-#####################################################################################################
-# Update this function so that it uses line of best fit
-#####################################################################################################
-    # Pad arrays if there are value errors
-    def fix(self, frame):
-        if len(self.leftWristPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in left_wrist")
-
-            if (len(self.leftWristPoints) < self.numFrames):
-                while len(self.leftWristPoints) != self.numFrames:
-                    self.leftWristPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.leftWristPoints) != self.numFrames:
-                self.leftWristPoints.pop()
-        
-        if len(self.leftElbowPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in left_elbow")
-
-            if (len(self.leftElbowPoints) < self.numFrames):
-                while len(self.leftElbowPoints) != self.numFrames:
-                    self.leftElbowPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.leftElbowPoints) != self.numFrames:
-                self.leftElbowPoints.pop()
-        
-        if len(self.leftShoulderPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in left_shoulder")
-            
-            if (len(self.leftShoulderPoints) < self.numFrames):
-                while len(self.leftShoulderPoints) != self.numFrames:
-                    self.leftShoulderPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.leftShoulderPoints) != self.numFrames:
-                self.leftShoulderPoints.pop()
-        
-        if len(self.leftHipPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in left_hip")
-            
-            if (len(self.leftHipPoints) < self.numFrames):
-                while len(self.leftHipPoints) != self.numFrames:
-                    self.leftHipPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.leftHipPoints) != self.numFrames:
-                self.leftHipPoints.pop()
-        
-        if len(self.leftKneePoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in left_knee")
-            
-            if (len(self.leftKneePoints) < self.numFrames):
-                while len(self.leftKneePoints) != self.numFrames:
-                    self.leftKneePoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.leftKneePoints) != self.numFrames:
-                self.leftKneePoints.pop()
-
-        if len(self.leftAnklePoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in left_ankle")
-            
-            if (len(self.leftAnklePoints) < self.numFrames):
-                while len(self.leftAnklePoints) != self.numFrames:
-                    self.leftAnklePoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.leftAnklePoints) != self.numFrames:
-                self.leftAnklePoints.pop()
-
-        if len(self.rightWristPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in right_wrist")
-            
-            if (len(self.rightWristPoints) < self.numFrames):
-                while len(self.rightWristPoints) != self.numFrames:
-                    self.rightWristPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.rightWristPoints) != self.numFrames:
-                self.rightWristPoints.pop()
-        
-        if len(self.rightElbowPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in right_elbow")
-            
-            if (len(self.rightElbowPoints) < self.numFrames):
-                while len(self.rightElbowPoints) != self.numFrames:
-                    self.rightElbowPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.rightElbowPoints) != self.numFrames:
-                self.rightElbowPoints.pop()
-        
-        if len(self.rightShoulderPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in right_shoulder")
-
-            if (len(self.rightShoulderPoints) < self.numFrames):
-                while len(self.rightShoulderPoints) != self.numFrames:
-                    self.rightShoulderPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.rightShoulderPoints) != self.numFrames:
-                self.rightShoulderPoints.pop()
-        
-        if len(self.rightHipPoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in right_hip")
-            
-            if (len(self.rightHipPoints) < self.numFrames):
-                while len(self.rightHipPoints) != self.numFrames:
-                    self.rightHipPoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.rightHipPoints) != self.numFrames:
-                self.rightHipPoints.pop()
-        
-        if len(self.rightKneePoints) != self.numFrames:
-            if self.shouldWarn:
-                print("Warning: point error in frame " + frame + " in right_knee")
-            
-            if (len(self.rightKneePoints) < self.numFrames):
-                while len(self.rightKneePoints) != self.numFrames:
-                    self.rightKneePoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.rightKneePoints) != self.numFrames:
-                self.rightKneePoints.pop()
-
-        if len(self.rightAnklePoints) != self.numFrames:
-            if self.shouldWarn:    
-                print("Warning: point error in frame " + frame + " in right_ankle")
-
-            if (len(self.rightAnklePoints) < self.numFrames):
-                while len(self.rightAnklePoints) != self.numFrames:
-                    self.rightAnklePoints.append({'x':0, 'y': 0, 'z': 0})
-            
-            while len(self.rightAnklePoints) != self.numFrames:
-                self.rightAnklePoints.pop()
-#####################################################################################################
-
 
     # Parse points
     def parse(self):
-        # Arrays for joint points
         leftUpper = []
         leftMiddle = []
         leftLower = []
@@ -286,7 +73,7 @@ class Parser:
         rightLower = []
 
         # Elbow joint
-        if self.joint == 'elbow':
+        if self.joint == 'Elbow':
             leftUpper = self.leftShoulderPoints
             leftMiddle = self.leftElbowPoints
             leftLower = self.leftWristPoints
@@ -295,7 +82,7 @@ class Parser:
             rightLower = self.rightWristPoints
 
         # Shoulder joint
-        if self.joint == 'shoulder':
+        if self.joint == 'Shoulder':
             leftUpper = self.leftElbowPoints
             leftMiddle = self.leftShoulderPoints
             leftLower = self.leftHipPoints
@@ -304,7 +91,7 @@ class Parser:
             rightLower = self.rightHipPoints
                 
         # Hip joint
-        if self.joint == 'hip':
+        if self.joint == 'Hip':
             leftUpper = self.leftShoulderPoints
             leftMiddle = self.leftHipPoints
             leftLower = self.leftKneePoints
@@ -313,7 +100,7 @@ class Parser:
             rightLower = self.rightKneePoints
         
         # Knee joint
-        if self.joint == 'knee':
+        if self.joint == 'Knee':
             leftUpper = self.leftHipPoints
             leftMiddle = self.leftKneePoints
             leftLower = self.leftAnklePoints
@@ -322,34 +109,4 @@ class Parser:
             rightLower = self.rightAnklePoints
         
         # Return a tuple with the left and right, upper, middle and lower points
-        return self.numFrames, leftUpper, leftMiddle, leftLower, rightUpper, rightMiddle, rightLower
-
-
-    # Save file with default filename
-    def save(self):
-        # Create default filename
-        uid = self.data['uid']
-        sid = self.data['sid']
-        clipNum = self.data['clipNum']
-        filename = 'uid-' + str(uid) + '-sid-' + str(sid) + '-clipNum-' + str(clipNum)
-
-        # Get keypoints in parsed file
-        keypoints = self.parse()
-
-        # Create a dictionary for the keypoints
-        keypointsDictionary = {
-            'numFrames': keypoints[0], 
-            'leftUpper': keypoints[1],
-            'leftMiddle': keypoints[2],
-            'leftLower': keypoints[3],
-            'rightUpper': keypoints[4],
-            'rightMiddle': keypoints[5],
-            'rightLower': keypoints[6]
-        }
-
-        # Save in a json file
-        if 'json' not in filename:
-            filename = filename + '.json'
-
-        with open(filename, 'w') as saveFile:
-            saveFile.write(json.dumps(keypointsDictionary, indent=2))
+        return leftUpper, leftMiddle, leftLower, rightUpper, rightMiddle, rightLower
--- chart/Visualise.py
+++ chart/Visualise.py
@@ -1,114 +1,45 @@
 # Imports
-import sys
 from . import Parser
 from . import Calculator
 from . import Plotter
+
+import sys
 import os
 
-from django.http import FileResponse, HttpResponse
-from django.contrib.staticfiles import finders
+import time
 
 
-def generate_plot(joint, interval, dimension, frame):
-    # Parse input file
-    filename_path = finders.find("frames-1-10.json")
-    parser = Parser.Parser(joint, filename_path, False, False)
-    numFrames, leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints = parser.parse()
+# Generate all plots to keep in memory
+def generate_plot_for_all_frames(joint, dimension, poseData):
+    all_frames = []
+    numFrames = len(poseData)
+    joint = joint.lower().capitalize()
+
+    # Parse pose data
+    parser = Parser.Parser(joint, poseData)
+    leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints = parser.parse()
 
     # Calculate angles
     calculator = Calculator.Calculator(numFrames, leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints)
     leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d = calculator.Calculate()
 
-    # Plot
-    plotter = Plotter.Plotter(numFrames, joint, interval, leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d)
+    print(f'Generating {numFrames} plots')
+    start = time.time()
 
+    for frame in range(0, len(poseData)):
+        generated_frame = generate_plot(joint, dimension, numFrames, frame, leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d)
+        all_frames.append(generated_frame)
+        print("Generated no." + str(frame))
+
+    print("Time taken: " + str((time.time() - start)/60))
+    return all_frames
+
+
+# Generate a single plot
+def generate_plot(joint, dimension, numFrames, frame, leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d):
     if dimension == '2d':
-        plotter.plot2d(frame)
-    elif dimension == '3d':
-        plotter.plot3d(frame)
-
-    plot_filename = "chart"
-
-    plotter.save(dimension, plot_filename)
-    
-    with open(plot_filename + "-" + dimension + ".png", 'rb') as f:
-        image_data = f.read()
-
-    response = HttpResponse(content_type='image/png')
-    response['Content-Disposition'] = f'filename={plot_filename}'
-    response.write(image_data)
-
-    return response
-
-# Program entry point
-if __name__ == '__main__' :
-    # Check for arguments error
-    if len(sys.argv) < 5:
-        print('Usage Error: joint interval dimension filename')
-        exit(1)
-
-    # Check file type
-    if not sys.argv[4].endswith('.json'):
-        print('Usage Error: requires json file')
-        exit(1)
-
-    # Check dimensions type
-    if not sys.argv[3].lower() == '2d' and not sys.argv[3].lower() == '3d':
-        print('Usage Error: dimension must be "2d" or "3d"')
-        exit(1)
-
-    # Get variables
-    joint = sys.argv[1]
-    interval = sys.argv[2]
-    dimension = sys.argv[3]
-    filename = sys.argv[4]
-
-    # Check argv
-    if '-h' in sys.argv:
-        print('Help')
-        print(' -h  : Print help')
-        print(' -i  : Ignore erroneous keypoints')
-        print(' -w  : Print warnings')
-        print(' -s  : Save parsed json data')
-        print(' -S  : Save visualisation graph')
-
-    warnings = False
-    if '-w' in sys.argv:
-        warnings = True
-
-    # Parse input file
-    ignore = False
-    if '-i' in sys.argv:
-        ignore = True
-
-    # Parse input file into form we need
-    # @param joint, filename, shouldIgnore, shouldWarn
-    parser = Parser.Parser(joint, filename, ignore, warnings)
-    # @return numFrames, leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints
-    numFrames, leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints = parser.parse()
-
-    # Calculate angles
-    # @param numFrames, leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints
-    calculator = Calculator.Calculator(numFrames, leftUpperPoints, leftMiddlePoints, leftLowerPoints, rightUpperPoints, rightMiddlePoints, rightLowerPoints)
-    # @return leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d
-    leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d = calculator.Calculate()
-
-    # Plot
-    # @param numFrames, joint, interval ,leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d
-    plotter = Plotter.Plotter(numFrames, joint, interval, leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw, left3d, right3d)
-    if dimension == '2d':
-        plotter.plot2d()
-    elif dimension == '3d':
-        plotter.plot3d()
-
-    if '-S' in sys.argv:
-        rawFilename, _ = os.path.splitext(os.path.basename(filename))
-        plotter.save(dimension, rawFilename + '_angle_visulisation')
+        data = Plotter.nplot2d(numFrames, frame, joint, leftJointRoll, leftJointPitch, leftJointYaw, rightJointRoll, rightJointPitch, rightJointYaw)
     else:
-        plotter.show()
-
-    # Check argv
-    if '-s' in sys.argv:
-        filenameIndex = sys.argv.index('-s') + 1
-        parser.save()
-
+        data = Plotter.nplot3d(numFrames, frame, joint, left3d, right3d)
+    
+    return data
