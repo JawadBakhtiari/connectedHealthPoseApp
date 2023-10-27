1b858d683976447325a2c9ce801f7ef30a4a3383
Leo KING <127772787+leoli88@users.noreply.github.com>
2023-10-27 22:15:22 +1100

    Got chart working!

--- static/js/chart.js
+++ static/js/chart.js
@@ -4,14 +4,6 @@
     process this input and display a graph of the corresponding clip.
 */
 
-import { getFullUrl } from './helper.js';
-
-
-function requestVisualisation(sessionId, clipNum) {
-    const url = getFullUrl(`/data/visualise2D/?sid=${sessionId}&clipNum=${clipNum}`);
-    window.location.href = url;
-}
-
 export const doVisualisation = (frameData, chartData) => {
     /* Code to display the visualisation (create a video display) */
     let currentFrame = 0;
@@ -36,7 +28,7 @@ export const doVisualisation = (frameData, chartData) => {
         const image = document.getElementById("animation");
         image.src = "data:image/png;base64," + frameBase64Img;
 
-        const chart = document.getElementById(chart)
+        const chart = document.getElementById("chart")
         chart.src =  "data:image/png;base64," + chartBase46Img
 
         currentFrame++;
@@ -48,17 +40,4 @@ export const doVisualisation = (frameData, chartData) => {
     setInterval(updateFrame, 100);  // Adjust the interval (in milliseconds) to control the animation speed
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    /*  When start button is clicked, send session id and clip number in request 
-        to backend. 
-    */
-    const sessionId = document.getElementById('session-id-input');
-    const clipNum = document.getElementById('clip-num-input');
-    const startButton = document.getElementById('start-visualisation-button');
-
-    startButton.addEventListener('click', function() {
-        requestVisualisation(sessionId.value, clipNum.value);
-    });
-
-    doVisualisation(frameData);
-});
+doVisualisation(frameData, chartData)
