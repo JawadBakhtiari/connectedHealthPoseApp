c0c76bc7986e40e9aa6c0e5786d7c9f490ff5ecd
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2024-05-22 17:32:01 +1000

    start work replacing expo cam

--- reactNativePoseAppClient/pages/SecondScreen.jsx
+++ reactNativePoseAppClient/pages/SecondScreen.jsx
@@ -1,52 +1,51 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-  Image,
-  Dimensions,
-  Modal,
-  Button,
-  TouchableOpacity,
-} from "react-native";
-import { Camera } from "expo-camera";
-import { GLView } from "expo-gl";
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 import * as tf from "@tensorflow/tfjs";
 import * as posedetection from "@tensorflow-models/pose-detection";
-import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
-import * as FileSystem from "expo-file-system";
 import * as jpeg from "jpeg-js";
-import * as MediaLibrary from "expo-media-library";
-import { encode, decode } from "base64-arraybuffer";
 import Axios from "axios";
-import { Fontisto } from "@expo/vector-icons";
+import { useCameraPermission, useCameraDevice, Camera, useFrameProcessor } from "react-native-vision-camera";
+import { loadTensorflowModel } from "react-native-fast-tflite";
+import { useResizePlugin } from "vision-camera-resize-plugin";
 
-const TensorCamera = cameraWithTensors(Camera);
 
-const IS_IOS = Platform.OS === "ios";
-
-// The size of the resized output from TensorCamera.
-const OUTPUT_TENSOR_WIDTH = 120;
-const OUTPUT_TENSOR_HEIGHT = OUTPUT_TENSOR_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
-
-export default function SecondScreen({ route, navigation }) {
-  const cameraRef = useRef(null);
-  const [tfReady, setTfReady] = useState(false);
+export default async function SecondScreen({ route, navigation }) {
+  // const poseDetection = await loadTensorflowModel('assets/movenetLightning.tflite');
+  // const model = poseDetection.state === 'loaded' ? poseDetection.model : null;
+  const model = null;
   const [recorded, setRecorded] = useState(false);
-  const [model, setModel] = useState(null);
-  //const [poses, setPoses] = useState([]);
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+  const { requestPermission } = useCameraPermission();
+  // const [cameraType, setCameraType] = useState('back');
+  const device = useCameraDevice('back');
   const rafId = useRef(null);
   const [fps, setFps] = useState(0);
-  let lastSendTime = Date.now();
   const poses = [];
   const tensorAsArray = [];
   const [activateEffect, setActivateEffect] = useState(false);
-  const [timer, setTimer] = useState(0);
-  const { uid } = route.params;
-  const { sid } = route.params;
-  const { code } = route.params;
+  const { resize } = useResizePlugin();
+  const { sid, code } = route.params;
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+    if (model == null) return
+
+    // Resize 4k Frame to 192x192x3 using vision-camera-resize-plugin
+    const resized = resize(frame, {
+        scale: {
+          width: 192,
+          height: 192,
+        },
+        pixelFormat: 'rgb',
+        dataType: 'uint8',
+    });
+
+    // Run model with given input buffer synchronously
+    const outputs = model.runSync([resized]);
+    console.log(outputs);
+
+    // TODO : now what ???
+
+}, [model])
 
   /**
    * This is a React useEffect hook that runs when the component is mounted.
@@ -62,7 +61,7 @@ export default function SecondScreen({ route, navigation }) {
     async function prepare() {
       rafId.current = null;
 
-      await Camera.requestCameraPermissionsAsync();
+      await requestPermission();
       await tf.ready();
       /**
        * Pose Detection Setup:
@@ -111,20 +110,20 @@ export default function SecondScreen({ route, navigation }) {
     };
   }, [activateEffect]);
 
-  useEffect(() => {
-    let interval;
-
-    if (tfReady) {
-      interval = setInterval(() => {
-        setTimer((prevTimer) => prevTimer + 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-      setTimer(0);
-    }
-
-    return () => clearInterval(interval);
-  }, [tfReady]);
+  //useEffect(() => {
+  //  let interval;
+//
+  //  if (tfReady) {
+  //    interval = setInterval(() => {
+  //      setTimer((prevTimer) => prevTimer + 1);
+  //    }, 1000);
+  //  } else {
+  //    clearInterval(interval);
+  //    setTimer(0);
+  //  }
+//
+  //  return () => clearInterval(interval);
+  //}, [tfReady]);
 
   const formatTime = (timeInSeconds) => {
     const hours = Math.floor(timeInSeconds / 3600);
@@ -146,7 +145,7 @@ export default function SecondScreen({ route, navigation }) {
 
     if (recorded == true) {
       // Send rest of data once user has ended recording and switch to next screen
-      sendLastData();
+      sendData(true);
       setRecorded(false);
       // navigation.navigate("Uploading", { language: "french " });
     }
@@ -199,39 +198,20 @@ export default function SecondScreen({ route, navigation }) {
     );
   };
 
-  const sendLastData = async () => {
+  const sendData = async (last = false) => {
     try {
       const response = Axios.post("http://" + code + "/data/frames/upload/", {
         // uid: "ahmad232",
         sid,
         // clipNum: "1",
-        clipFinished: true,
+        clipFinished: last,
         poses,
         tensorAsArray,
       });
       // Empty Data
       poses.splice(0, poses.length);
       tensorAsArray.splice(0, tensorAsArray.length);
-      console.log("sending last data:");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const sendData = async () => {
-    try {
-      const response = Axios.post("http://" + code + "/data/frames/upload/", {
-        // uid: "ahmad232",
-        sid,
-        // clipNum: "1",
-        clipFinished: false,
-        poses,
-        tensorAsArray,
-      });
-      // Empty Data
-      poses.splice(0, poses.length);
-      tensorAsArray.splice(0, tensorAsArray.length);
-      console.log("sending data:");
+      console.log(last ? "sending last data" : "sending data");
     } catch (err) {
       console.log(err);
     }
@@ -294,107 +274,18 @@ export default function SecondScreen({ route, navigation }) {
     }
   };
 
-  const handleSwitchCameraType = () => {
-    if (cameraType === Camera.Constants.Type.front) {
-      setCameraType(Camera.Constants.Type.back);
-    } else {
-      setCameraType(Camera.Constants.Type.front);
-    }
-  };
+  // const handleSwitchCameraType = () => {
+  //   cameraType === 'front' ? setCameraType('back') : setCameraType('front');
+  // };
 
-  const renderRecordButton = () => {
-    return (
-      <View style={styles.recordButton}>
-        <TouchableOpacity style={styles.recordB} onPress={handleTf}>
-          <View style={[styles.inner, tfReady && styles.activeInner]}></View>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
-  const renderExitButton = () => {
-    return (
-      <View
-        style={styles.ExitButton}
-        onTouchEnd={() => navigation.navigate("Home", { language: "french" })}
-      >
-        <Image
-          source={require("./assets/exitB.png")}
-          style={styles.ExitImage}
-        />
-      </View>
-    );
-  };
-
-  const renderSwitchCamButton = () => {
-    return (
-      <View style={styles.SwitchButton} onTouchEnd={handleSwitchCameraType}>
-        <Fontisto name="arrow-swap" size={24} color="white" />
-      </View>
-    );
-  };
-
-  const rendertimer = () => {
-    return (
-      <View style={styles.timercontain}>
-        {tfReady ? (
-          <View style={styles.activeTimer}>
-            <Text style={styles.timerText}>{formatTime(timer)}</Text>
-          </View>
-        ) : (
-          <View style={styles.notactiveTimer}>
-            <Text style={styles.timerText}>{formatTime(timer)}</Text>
-          </View>
-        )}
-      </View>
-    );
-  };
-
-  if (!tfReady) {
-    return (
-      <View style={styles.container}>
-        <View style={styles.top}>
-          {renderExitButton()}
-          <View style={styles.timercontain}>{rendertimer()}</View>
-          <View style={{ flex: 1 }}></View>
-        </View>
-        <Camera ref={cameraRef} style={styles.camera} type={cameraType} />
-        <View style={styles.bottom}>
-          <View style={{ flex: 1 }}></View>
-          <View style={styles.recordcontain}>{renderRecordButton()}</View>
-          <View style={styles.switchcontain}>{renderSwitchCamButton()}</View>
-        </View>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <View style={styles.top}>
-          <View style={{ flex: 1 }}></View>
-          <View style={styles.timercontain}>{rendertimer()}</View>
-          <View style={{ flex: 1 }}></View>
-        </View>
-        <TensorCamera
-          ref={cameraRef}
-          style={styles.tcamera}
-          autorender={true}
-          type={cameraType}
-          // tensor related props
-          resizeWidth={OUTPUT_TENSOR_WIDTH}
-          resizeHeight={OUTPUT_TENSOR_HEIGHT}
-          resizeDepth={3}
-          onReady={handleCameraStream}
-        />
-        {/*renderPose()*/}
-        {renderFps()}
-        <View style={styles.bottom}>
-          <View style={{ flex: 1 }}></View>
-          <View style={styles.recordcontain}>{renderRecordButton()}</View>
-          <View style={styles.switchcontain}>{renderSwitchCamButton()}</View>
-        </View>
-      </View>
-    );
-  }
+  return (
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        frameProcessor={frameProcessor}
+      />
+  );
 }
 
 const styles = StyleSheet.create({
