c10303050a53707cf82f28e2d6c4d425f2d15598
Ahmad Jawad Bakhtiari <aj.bakhti@gmail.com>
2024-05-25 22:52:31 +1000

    Fixed Circle button

--- reactNativePoseAppClient/pages/SecondScreen.jsx
+++ reactNativePoseAppClient/pages/SecondScreen.jsx
@@ -484,7 +484,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#9e1919",
     width: "100%",
     height: "100%",
-    borderRadius: "50%",
+    borderRadius: 50,
     transitionProperty: "all",
     transitionDuration: 200,
     transitionTimingFunction: "ease",
