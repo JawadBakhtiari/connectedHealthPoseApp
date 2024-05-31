a3aa55860ad4a8c36cf2ee1b5e221fdfd1a3b0c4
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2024-05-31 12:04:35 +1000

    start work writing script to find capture times for livability lab motion capture

--- validation/format.py
+++ validation/format.py
@@ -2,23 +2,31 @@
 
 # Play around with the formatting of data captured by the livability lab motion capture system
 
-import csv
+import pandas as pd
 
-csv_file_path = 'exampleData/example.csv'
+filepath = 'exampleData/firstSampleSitToStand/livabilityLabPoses.csv'
 
-def csv_to_dict(filepath: str):
-  '''
-    Convert csv motion capture data into a list of dictionaries.
-    Each dictionary contains frame number, time captured, and x,y,z,w values.
-  '''
-  with open(filepath) as f:
+def clean_data(filepath: str):
+  '''Currently messy - will clean up once properly implemented.'''
+  baseline_data = pd.read_csv(filepath, skiprows=6).iloc[:, 1:]
 
-    # Skip headers
-    for _ in range(6):
-      next(f)
+  data = pd.read_csv(filepath, skiprows=3)
+  headers = data.columns.tolist()
+  unlabeled_cols = [col for col in headers if 'Unlabeled' in col]
+  unlabeled_cols.append('Name')
+  filtered_data = data[unlabeled_cols]
+  next_row = filtered_data.iloc[0]
+  position_cols_only = [col for col in unlabeled_cols if 'Rotation' not in next_row[col]]
+  result = filtered_data[position_cols_only].iloc[3:]
 
-    # Return frame data
-    return list(csv.DictReader(f))
+  for _, row in result.iterrows():
+    if row.drop('Name').isnull().all():
+      timestamp = row['Name']
+      all_other_vals_at_this_time = baseline_data[baseline_data.iloc[:, 0] == float(timestamp)]
+      all_null = all_other_vals_at_this_time.iloc[:, 1:].isnull().all(axis=1).all()
+      if not all_null:
+        # make sure that data capture has not faulted at this time
+        print("Time when all unlabeled markers are null (and not all markers are null):", timestamp)
+        break
 
-
-print(csv_to_dict(csv_file_path))
+clean_data(filepath)
