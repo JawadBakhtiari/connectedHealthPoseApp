837bb63b072c32c15548e73c6eae3f9c3437b883
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2024-06-05 15:12:08 +1000

    continue work on validation script - finding start of exercise

--- validation/format.py
+++ validation/format.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
-
 # Play around with the formatting of data captured by the livability lab motion capture system
 
 import pandas as pd
 
 filepath = 'exampleData/firstSampleSitToStand/livabilityLabPoses.csv'
+NUM_SYNCHRONISATION_MARKERS = 5
 
-def clean_data(filepath: str):
-  '''Currently messy - will clean up once properly implemented.'''
-  baseline_data = pd.read_csv(filepath, skiprows=6).iloc[:, 1:]
-
+def preprocess(filepath: str) -> pd.DataFrame:
+  '''Remove null rows from livibility lab data capture.'''
   data = pd.read_csv(filepath, skiprows=3)
+  rows_with_data = ~data.iloc[:, 2:].isna().all(axis=1)
+  return data[rows_with_data]
+
+def clean_data(filepath: str) -> None:
+  '''Currently messy - will clean up once properly implemented.'''
+  data = preprocess(filepath)
   headers = data.columns.tolist()
-  unlabeled_cols = [col for col in headers if 'Unlabeled' in col]
-  unlabeled_cols.append('Name')
-  filtered_data = data[unlabeled_cols]
+  unlabelled_and_timestamp_cols = [col for col in headers if 'Unlabeled' in col or col == 'Name']
+  filtered_data = data[unlabelled_and_timestamp_cols]
   next_row = filtered_data.iloc[0]
-  position_cols_only = [col for col in unlabeled_cols if 'Rotation' not in next_row[col]]
+  position_cols_only = [col for col in unlabelled_and_timestamp_cols if 'Rotation' not in next_row[col]]
   result = filtered_data[position_cols_only].iloc[3:]
 
   for _, row in result.iterrows():
-    if row.drop('Name').isnull().all():
+    print(row)
+    print(len(list(filter(lambda x: x, list(row.drop('Name').isnull())))))
+    num_undetected_markers = list(filter(lambda x: x, list(row.drop('Name').isnull()))).sum()
+    if num_undetected_markers > NUM_SYNCHRONISATION_MARKERS * 3:
       timestamp = row['Name']
-      all_other_vals_at_this_time = baseline_data[baseline_data.iloc[:, 0] == float(timestamp)]
-      all_null = all_other_vals_at_this_time.iloc[:, 1:].isnull().all(axis=1).all()
-      if not all_null:
-        # make sure that data capture has not faulted at this time
-        print("Time when all unlabeled markers are null (and not all markers are null):", timestamp)
-        break
+      print("Time when all unlabeled markers are null (and not all markers are null):", timestamp)
+      break
 
 clean_data(filepath)
---
+++ validation/notes.md
@@ -0,0 +1,4 @@
+### Notes from chatting with Rahm
+
+  - bone types are 'keypoints', marker and bone marker types are not 'keypoints'
+  - use position and not rotation
