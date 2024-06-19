43129a04f937bf39bece750986094691ffeb344d
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2024-06-19 14:27:22 +1000

    add another possible validation technique

--- validation/validate.py
+++ validation/validate.py
@@ -4,6 +4,8 @@ import numpy as np
 
 '''Trying out some different methods for validation.'''
 
+FACTOR = 10
+
 mobile_ys = [
   98.73078761334244,
   64.96097916316998,
@@ -31,7 +33,7 @@ lab_ys = [
 
 def scale_mobile_ys(mobile_ys: list) -> list:
   '''Scale up mobile data y values so values are in a similar range to lab data.'''
-  return list(map(lambda y : y * 10, mobile_ys))
+  return list(map(lambda y : y * FACTOR, mobile_ys))
 
 
 def calc_percentage_diff(first: float, second: float) -> float:
@@ -55,6 +57,19 @@ def compare_percentage_change(mobile_percentage_changes: list, lab_percentage_ch
   average_difference = reduce(lambda a, b: a + b, percentage_differences) / len(percentage_differences)
   return average_difference
 
+def compare_absolute_differences(mobile_ys: list, lab_ys: list) -> float:
+  mobile_ys = scale_mobile_ys(mobile_ys)
+  last = None
+  diffs = []
+  for my, ly in list(zip(mobile_ys, lab_ys)):
+    if last:
+      last_my, last_ly = last
+      diff_lab = abs(last_ly - ly)
+      diff_mobile = abs(last_my - my)
+      diffs.append(abs(diff_lab - diff_mobile))
+    last = (my, ly)
+  return reduce(lambda a, b: a + b, diffs) / len(diffs) 
+
 
 def huber_loss(y_true, y_pred, delta):
     residual = y_true - y_pred
@@ -70,22 +85,24 @@ mobile_percentage_changes = calc_percentage_changes(mobile_ys)
 lab_percentage_changes = calc_percentage_changes(lab_ys)
 delta = 0.6 # for huber loss
 
+average_absolute_diff = compare_absolute_differences(mobile_ys, lab_ys)
 average_percentage_diff = compare_percentage_change(mobile_percentage_changes, lab_percentage_changes)
 hl = huber_loss(np.array(lab_percentage_changes), np.array(mobile_percentage_changes), delta)
 
-print("==========================")
-print("====PERCENTAGE CHANGES====")
-print("==========================")
+print("========================================")
+print("===========PERCENTAGE CHANGES===========")
+print("========================================")
 print("mobile percentage changes:", mobile_percentage_changes)
 print("lab percentage changes:", lab_percentage_changes)
-print("==========================\n")
+print("========================================\n")
 
-print("==========================")
-print("=========RESULTS==========")
-print("==========================")
-print("average diff:", average_percentage_diff)
-print("huber loss:", hl)
-print("==========================")
+print("========================================")
+print("================RESULTS=================")
+print("========================================")
+print("average absolute diff:", f"\t\t{average_absolute_diff:.2f}cm")
+print("average percentage diff:", f"\t{average_percentage_diff:.2f}%")
+print("huber loss:", f"\t\t\t{hl:.2f}%")
+print("========================================")
 
 #####################################################################################################
 #####################################################################################################
