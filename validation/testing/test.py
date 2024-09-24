52383ab2df8572b1fdbba4688749c9eebdf357d0
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2024-09-24 14:52:18 +1000

    add visualisation of exercise checks, and complete exercise checks for 360 spin and sit to stand exercises

--- validation/testing/test.py
+++ validation/testing/test.py
@@ -4,51 +4,92 @@
 
 '''Currently testing out calculating when a 360 degree spin has been executed'''
 
-import json
+# import json
 import heapq
+import math
 
 def same_sign(a, b):
     return a * b > 0
 
-with open('../be_pose_estimation/data/results/20240904/uncalibrated_360_spin_test_thunder.json') as f:
-    poses = json.load(f)
+# with open('../be_pose_estimation/data/results/20240904/uncalibrated_360_spin_test_thunder.json') as f:
+#     poses = json.load(f)
 
-NUM_SHOULDER_WIDTHS = 10
-SHOULDER_WIDTH_DIFF_TOLERANCE = 0.01
+def run_360_check(poses) -> float:
+    NUM_SHOULDER_WIDTHS = 10
+    SHOULDER_WIDTH_DIFF_TOLERANCE = 0.01
 
-last_shoulder_dist = None
-pose_index = 0
-shoulder_dists = []
+    last_shoulder_dist = None
+    pose_index = 0
+    shoulder_dists = []
 
+    # loop for getting shoulder width before first turn
+    while pose_index < len(poses):
+        pose = {kp['name']: kp for kp in poses[pose_index]['keypoints']}
+        shoulder_dist = pose['left_shoulder']['x'] - pose['right_shoulder']['x']
+        if last_shoulder_dist != None and not same_sign(shoulder_dist, last_shoulder_dist):
+            # turning point
+            break
+        if len(shoulder_dists) < NUM_SHOULDER_WIDTHS:
+            heapq.heappush(shoulder_dists, shoulder_dist)
+        elif shoulder_dist > shoulder_dists[0]:
+            heapq.heapreplace(shoulder_dists, shoulder_dist)
+        last_shoulder_dist = shoulder_dist
+        pose_index += 1
 
-# loop for getting shoulder width before first turn
-while pose_index < len(poses):
-    pose = {kp['name']: kp for kp in poses[pose_index]['keypoints']}
-    shoulder_dist = pose['left_shoulder']['x'] - pose['right_shoulder']['x']
+    shoulder_width = shoulder_dists[NUM_SHOULDER_WIDTHS // 2]
 
-    if last_shoulder_dist != None and not same_sign(shoulder_dist, last_shoulder_dist):
-        # turning point
-        break
+    while pose_index < len(poses):
+        pose = {kp['name']: kp for kp in poses[pose_index]['keypoints']}
+        shoulder_dist = pose['left_shoulder']['x'] - pose['right_shoulder']['x']
+        if abs(shoulder_dist - shoulder_width) <= SHOULDER_WIDTH_DIFF_TOLERANCE:
+            return poses[pose_index]['time_since_start']
+        last_shoulder_dist = shoulder_dist
+        pose_index += 1
 
-    if len(shoulder_dists) < NUM_SHOULDER_WIDTHS:
-        heapq.heappush(shoulder_dists, shoulder_dist)
-    elif shoulder_dist > shoulder_dists[0]:
-        heapq.heapreplace(shoulder_dists, shoulder_dist)
+    return poses[pose_index]['time_since_start'] + 1
 
-    last_shoulder_dist = shoulder_dist
-    pose_index += 1
+def calc_joint_angle(initial_side: dict, vertex: dict, terminal_side: dict):
+    v1 = (vertex['x'] - initial_side['x'], vertex['y'] - initial_side['y'])
+    v2 = (vertex['x'] - terminal_side['x'], vertex['y'] - terminal_side['y'])
 
+    dot_product = v1[0] * v2[0] + v1[1] * v2[1]
+    magnitude_v1 = math.sqrt(v1[0] ** 2 + v1[1] ** 2)
+    magnitude_v2 = math.sqrt(v2[0] ** 2 + v2[1] ** 2)
 
-shoulder_width = shoulder_dists[NUM_SHOULDER_WIDTHS // 2]
+    cos_angle = dot_product / (magnitude_v1 * magnitude_v2)
+    angle_radians = math.acos(cos_angle)
+    angle_degrees = math.degrees(angle_radians)
+    return angle_degrees
 
+def sts_check(poses, target) -> float:
+    check_seated = True
+    count = -1
+    for pose in poses:
+        time_since_start = pose['time_since_start']
+        pose = {kp['name']: kp for kp in pose['keypoints']}
+        wrist_height = pose['left_wrist']['y']
+        hip_height = pose['left_hip']['y']
+        if (wrist_height > hip_height):
+            # Hands must be on shoulders throughout exercise.
+            # This seems backwards, but this check is checking that hands are above hips
+            # or else the rep count is reset.
+            count = -1
+            check_seated = True
+        knee_flexion = calc_joint_angle(pose['left_ankle'], pose['left_knee'], pose['left_hip'])
+        if check_seated:
+            if knee_flexion <= 90:
+                check_seated = False
+                count += 1
+            if count == target:
+                return time_since_start
+        elif knee_flexion >= 145:
+            check_seated = True
+        print(count)
+    return poses[-1]['time_since_start'] + 1
 
-while pose_index < len(poses):
-    pose = {kp['name']: kp for kp in poses[pose_index]['keypoints']}
-    shoulder_dist = pose['left_shoulder']['x'] - pose['right_shoulder']['x']
-    if abs(shoulder_dist - shoulder_width) <= SHOULDER_WIDTH_DIFF_TOLERANCE:
-        print(f'time taken to complete turn -> {poses[pose_index]['time_since_start']:.2f} seconds')
-        break
-    last_shoulder_dist = shoulder_dist
-    pose_index += 1
+def run_single_sts_check(poses) -> float:
+    return sts_check(poses, 1)
 
+def run_five_sts_check(poses) -> float:
+    return sts_check(poses, 5)
 
--- validation/visualise.py
+++ validation/visualise.py
@@ -4,11 +4,21 @@ import json
 import cv2
 from time import sleep
 from be_pose_estimation.models.movenet_thunder import MovenetThunder as model
+from testing.test import run_single_sts_check
 
-with open('be_pose_estimation/data/results/20240904/uncalibrated_360_spin_test_thunder.json') as f:
+POSE_COLOR = (0, 0, 255)
+SUCCESS_COLOR = (0, 255, 0)
+TIME_FONT = cv2.FONT_HERSHEY_SIMPLEX
+TIME_POS = (20, 100)
+TIME_AFTER_SUCCESS_POS = (20, 200)
+TIME_SCALE = 3
+TIME_COLOR = (255, 255, 255)
+TIME_THICKNESS = 3
+TIME_LINE_TYPE = cv2.LINE_AA
+CAP = cv2.VideoCapture('example_data/random/sts_fail.MOV')
+with open('be_pose_estimation/data/results/20240904/uncalibrated_sts_fail_thunder.json') as f:
     POSES = json.load(f)
-
-CAP = cv2.VideoCapture('example_data/random/360_spin_test.mp4')
+EXERCISE_COMPLETED_TIME = run_single_sts_check(POSES)
 
 for pose in POSES:
     ret, frame_image = CAP.read()
@@ -19,16 +29,26 @@ for pose in POSES:
     frame_dims = (frame_width, frame_height)
     overlay_image = frame_image.copy()
 
+    time_since_start = CAP.get(cv2.CAP_PROP_POS_MSEC) / 1000
+    if time_since_start >= EXERCISE_COMPLETED_TIME:
+        POSE_COLOR = SUCCESS_COLOR
+        cv2.putText(overlay_image, f'{EXERCISE_COMPLETED_TIME:.2f}', TIME_POS, TIME_FONT, TIME_SCALE, SUCCESS_COLOR, TIME_THICKNESS, TIME_LINE_TYPE)
+        cv2.putText(overlay_image, f'{time_since_start:.2f}', TIME_AFTER_SUCCESS_POS, TIME_FONT, TIME_SCALE, TIME_COLOR, TIME_THICKNESS, TIME_LINE_TYPE)
+    else:
+        cv2.putText(overlay_image, f'{time_since_start:.2f}', TIME_POS, TIME_FONT, TIME_SCALE, TIME_COLOR, TIME_THICKNESS, TIME_LINE_TYPE)
+
     keypoints = [model.get_pixel_coordinate((kp['x'], kp['y']), frame_dims) for kp in pose['keypoints']]
 
     # Visualize the keypoints and connections
     for kp in keypoints:
-        cv2.circle(overlay_image, kp, radius=2, color=(0, 0, 255), thickness=7)
+        cv2.circle(overlay_image, kp, radius=2, color=POSE_COLOR, thickness=7)
+
+    # Display time since start of video
 
     for joint1, joint2 in model.joint_connections():
         pt1 = keypoints[joint1]
         pt2 = keypoints[joint2]
-        cv2.line(overlay_image, pt1, pt2, (0, 0, 255), 2)
+        cv2.line(overlay_image, pt1, pt2, POSE_COLOR, 2)
 
     cv2.imshow('Pose Estimation', overlay_image)
     sleep(0.05)
