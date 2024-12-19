b715017ffeda0386c4f6c1d50358bf36ee810415
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2024-12-19 15:33:47 +1100

    remove unecessary file

--- djangoWebAppStaff/data/datastore/const.py
+++
@@ -1,6 +0,0 @@
-# Azure blob storage constants
-AZ_ACCOUNT_NAME = "connectedhealth"
-AZ_ACCOUNT_KEY = "wq4q+wFN99NgDz0kkYdbTAArr5zrJH0x2yG9EFKgO13QYFlqrlX/9JvQRrKCEFAMQ77koN+oEP28+ASty7DtRA=="
-AZ_CON_STR = f"DefaultEndpointsProtocol=https;AccountName={AZ_ACCOUNT_NAME};AccountKey={AZ_ACCOUNT_KEY};EndpointSuffix=core.windows.net"
-AZ_POSES_CONTAINER_NAME = "poses"
-AZ_CLIPS_CONTAINER_NAME = "videos"
